9e8cedfbda3ae168fe31758fed40c3f7a75ca116
myfanwybr <m.brownrobinson@hotmail.com>
2021-01-16 20:19:28 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,14 +20,10 @@ function runEnter(){
 
     var input =d3.select("#input-feild");
 
-    //var inputValue = document.getElementById("input-feild").value
     var inputValue=input.property("value");
 
-    var filteredData=tableData.filter(date=> date.datetime==inputValue)
+    var filteredData=tableData.filter(date=> new Date (date.datetime).toDateString()=== new Date(inputValue).toDateString());
 
-    var map= tableData.map(date=> date.datetime==inputValue)
-
-    //if map == false
 
     filteredData.forEach(function(aliens){
             var row=tbody.append("tr")
@@ -43,36 +39,3 @@ function runEnter(){
 }
 
     
-    
-
-
-// // append table
-// var row= tbody.append("tr")
-// var cell=row.append("td").text(filteredData)
-
-
-// //loop through data 
-// tableData.forEach(function(aliens){
-//     var row=tbody.append("tr")
-
-//     //select objects using . Entries
-//     Object.entries(aliens).forEach(function([key, value]){
-//         var cell=row.append("td")
-//         cell.text(value)
-//     })
-// })
-
-// create function to handle button if clicked
-
-// button.on("click", function () {tableData.forEach(function(aliens){
-//         var row=tbody.append("tr")
-
-//         //select objects using . Entries
-//         Object.entries(aliens).forEach(function([key, value]){
-//             var cell=row.append("td")
-//             cell.text(value)
-//     })
-// })
-// })
-
-//on click filter
